f253e47f5a780987a7c211319e000257277b88dc
Matteo Gozzini <matteogozzini@students.berkeley.net>
2017-05-31 09:54:38 -0700

    hi

--- www/Drones/js/pak.js
+++ www/Drones/js/pak.js
@@ -46,7 +46,7 @@ function myMap(result) {
     var mapProp = {
         //lat and long of pakistan
         center:new google.maps.LatLng(30.3753, 69.3451),
-        zoom: 5,
+        zoom: 5
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     //get all the lat and lon coordinates into coordinates array
